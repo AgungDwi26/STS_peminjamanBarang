a54502b90ced38affdbfade3bcb8e7f5d834bece
AgungDwi26 <dwiagungprayogo26@gmail.com>
2024-03-14 06:06:16 +0700

    finish

---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<ul class="navbar-nav bg-gray-900 sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <a class="sidebar-brand d-flex align-items-center justify-content-center">
+                <div class="sidebar-brand-text mx-3">Peminjaman Barang <sup></sup></div>
+            </a>
+
+            <hr class="sidebar-divider my-0">
+
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-home"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="user.php">
+                    <i class="fas fa-fw fa-users"></i>
+                    <span>User</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="barang.php">
+                    <i class="fas fa-fw fa-briefcase"></i>
+                    <span>Barang</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="datapeminjaman.php">
+                    <i class="fas fa-fw fa-database"></i>
+                    <span>Data Peminjaman</span></a>
+            </li>
+            
+            <hr class="sidebar-divider d-none d-md-block">
+</ul>
